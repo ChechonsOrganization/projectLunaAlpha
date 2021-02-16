d9b77249fbac90df288f1b8fd96174aab973d190
Sergio Ramirez <sergio.04.ramirez@gmail.com>
2021-02-15 21:47:31 -0300

    Update the landing folder permissions

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+""" 
+    Version alpha of this app, may be changed completly in the future
+    This is the first version from the landing page, it could be only
+    a view for now. Until get new ideas to the project Luna instead.
+"""
