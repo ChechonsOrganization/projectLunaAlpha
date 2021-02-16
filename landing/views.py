6bc9379778b064cecef5caabd71e6b6a7348060c
Sergio Ramirez <sergio.04.ramirez@gmail.com>
2021-02-15 22:57:53 -0300

    created new files and updates

--- landing/views.py
+++ landing/views.py
@@ -7,3 +7,5 @@ from django.shortcuts import render
     This is the first version from the landing page, it could be only
     a view for now. Until get new ideas to the project Luna instead.
 """
+
+                           
